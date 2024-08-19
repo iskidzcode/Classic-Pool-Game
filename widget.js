c795974c34fdd3eeca6d3bbb91922aaefade2814
Roshaan <broshaan66@gmail.com>
2024-08-19 17:34:44 +0500

    Update widget.js

--- widget.js
+++ widget.js
@@ -3,5 +3,10 @@ console.log('Widget script loaded');
 var script = document.createElement('script');
 script.src = 'https://adsreference.com/widget.js?id=1724069896873659000';
 script.defer = true;
+script.onload = function() {
+var widget = document.createElement('div');
+widget.id = 'widget';
+document.getElementById('widgetContainer').appendChild(widget);
+};
 document.head.appendChild(script);
 })();
